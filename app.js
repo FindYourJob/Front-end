59ddaa2530b9d3f44769fcd491a10464d0ecda56
Melissa BR <melissa.benali-richard@etu.utc.fr>
2015-06-02 00:22:13 +0200

    exploitation du fichier json pour faire apparaitre les points sur la geomap

--- app.js
+++ app.js
@@ -1,84 +1,3 @@
-// WORKING
-/*
-//Define an angular module for our app
-var FYJApp = angular.module('FYJApp', ['ngRoute']);
-
-//Define Routing for app
-//Uri /AddNewOrder -> template AddOrder.html and Controller AddOrderController
-//Uri /ShowOrders -> template ShowOrders.html and Controller AddOrderController
-FYJApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/Accueil', {
-	templateUrl: 'templates/accueil.html',
-	controller: 'AccueilController'
-      }).
-      when('/Stats-Annonces', {
-	templateUrl: 'templates/stats-annonces.html',
-	controller: 'AccueilController'
-      }).
-      when('/Graphe-Job-Company', {
-	templateUrl: 'templates/graphes-job-company.html',
-	controller: 'GraphesJCController'
-      }).
-      otherwise({
-	redirectTo: '/Accueil'
-      });
-}]);
-
-FYJApp.controller("GraphesJCController", function($scope){
-	$.ajax( {
-		type: "GET",
-		//url: "http://www.trendyjobs.fr/backend/getJobAdverts/1",
-		//TODO replace with trendyjobs adress
-		url: "jobs.json",
-		dataType: "json",
-		success: function(jobList){
-			console.log(jobList);
-			$.getScript('scripts/filters/filters.js', function(){
-				$.getScript('scripts/graphManager.js', function() {
-					$.getScript('scripts/Model/nodeFactory.js', function(){
-						for(i=0;i<jobList.length;++i) {
-							jobList[i].nodeType = 'job';
-							TrendyJob.Filters.fillFilters(jobList[i]);
-							var gm = GraphManager.getInstance();
-							gm.addNode(jobList[i]);
-							var e = TrendyJob.Model.NodeFactory.getInstance().getEdge(jobList[i], 'company');
-							//gm.addEdge(e);
-						}
-						$.getScript('scripts/d3modules/graph.js', function(){
-							D3.printGraph();
-						});
-						TrendyJob.Filters.createPageFilters(["job"]);
-					});
-				});
-			});
-		},
-		fail: function(){
-			console.log("FAILED");
-		}
-	});
-
-});
-
-
-FYJApp.controller('AddOrderController', function($scope) {
-	
-	$scope.message = 'This is Add new order screen';
-	alert("coucou");
-	
-});
-
-
-FYJApp.controller('ShowOrdersController', function($scope) {
-
-	$scope.message = 'This is Show orders screen';
-	alert("coucou");
-
-});
-*/
-
-
 //Define an angular module for our app
 var FYJApp = angular.module('FYJApp', ['ngRoute']);
 
@@ -171,7 +90,8 @@ var map = new google.maps.Map(d3.select("#map").node(), {
 });
 
 // Load the station data. When the data comes back, create an overlay.
-d3.json("convertcsvShort.json", function(data) {
+d3.json("jobsWithLatLong.json", function(data) {
+  console.log(data);
   var overlay = new google.maps.OverlayView();
 
   // Add the container when the overlay is added to the map.
@@ -203,10 +123,10 @@ d3.json("convertcsvShort.json", function(data) {
           .attr("x", padding + 7)
           .attr("y", padding)
           .attr("dy", ".31em")
-          .text(function(d) { return d.value["libelle"]; });
+          .text(function(d) { return d.value["title"]; });
 
       function transform(d) {
-        d = new google.maps.LatLng(d.value["longitude"], d.value["latitude"]);
+        d = new google.maps.LatLng(d.value["lat"], d.value["long"]);
         console.log(d);
         d = projection.fromLatLngToDivPixel(d);
         console.log(d);
